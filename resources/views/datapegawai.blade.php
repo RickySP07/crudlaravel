08b9a218cfc2be7705c7dc1dbe990ebc616c87f9
Ricky Surya Permana <rickypermana0702@gmail.com>
2022-05-07 20:32:39 +0800

    first commit

---
+++ resources/views/datapegawai.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <!-- Bootstrap CSS -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+            crossorigin="anonymous"
+        />
+        {{-- Link CSS --}}
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css"
+            integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
+        <title>LatihanCrud</title>
+    </head>
+    <body>
+        <h1 class="text-center mt-3 mb-4">Data Pegawai</h1>
+        <div class="container">
+            <a href="/tambahpegawai" class="btn btn-success">Tambah Data +</a>
+            <div class="row g-3 align-items-center mt-2">
+                <div class="col-auto">
+                    <form action="/pegawai" method="GET">
+                        <input
+                        type="search"
+                        id="inputPassword6"
+                        class="form-control"
+                        aria-describedby="passwordHelpInline"
+                        name="search"
+                        placeholder="Search Data"
+                    />
+                    </form>  
+                </div>
+                <div class="col-auto">
+                    <a href="/exportpdf" class="btn btn-info">Export PDF</a>
+                </div>
+
+            </div>
+            <div class="row">
+                {{-- @if ($message = Session::get('success'))
+                <div class="alert alert-success mt-3" role="alert">
+                    {{ $message }}
+                </div>
+                @endif --}}
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Foto</th>
+                            <th scope="col">Jenis Kelamin</th>
+                            <th scope="col">No Telepon</th>
+                            <th scope="col">Dibuat</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php $no = 1; @endphp @foreach ($data as $index => $row)
+                        <tr>
+                            <th scope="row">
+                                {{ $index + $data->firstItem() }}
+                            </th>
+                            <td>{{ $row->nama }}</td>
+                            <td>
+                                <img
+                                    src="{{ asset('fotopegawai/'.$row->foto) }}"
+                                    alt=""
+                                    style="width: 40px"
+                                />
+                            </td>
+                            <td>{{ $row->jeniskelamin }}</td>
+                            <td>{{ $row->notelpon }}</td>
+                            <td>{{ $row->created_at->format('D M Y') }}</td>
+                            <td>
+                                <a
+                                    href="/tampilkandata/{{ $row->id }}"
+                                    class="btn btn-warning"
+                                >
+                                    Edit
+                                </a>
+                                <a
+                                    href="#"
+                                    class="btn btn-danger delete"
+                                    data-id="{{ $row->id }}"
+                                    data-nama="{{ $row->nama }}"
+                                >
+                                    Delete
+                                </a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                {{ $data->links() }}
+            </div>
+        </div>
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"
+        ></script>
+        <script
+            src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous"
+        ></script>
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
+            integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        ></script>
+    </body>
+    <script>
+        $(".delete").click(function () {
+            var pegawaiid = $(this).attr("data-id");
+            var nama = $(this).attr("data-nama");
+            swal({
+                title: "Apakah Anda Yakin?",
+                text:
+                    "Kamu akan menghapus data pegawai dengan nama " +
+                    nama +
+                    " ",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            }).then((willDelete) => {
+                if (willDelete) {
+                    window.location = "/delete/" + pegawaiid + " ";
+                    swal("Data Berhasil Dihapus", {
+                        icon: "success",
+                    });
+                } else {
+                    swal("Data tidak jadi dihapus");
+                }
+            });
+        });
+    </script>
+    <script>
+        @if(Session::has('success'))
+            toastr.success('{{ Session::get('success') }}')
+        @endif
+    </script>
+</html>
---
+++ resources/views/tampildata.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <!-- Bootstrap CSS -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+            crossorigin="anonymous"
+        />
+
+        <title>LatihanCrud</title>
+    </head>
+    <body>
+        <h1 class="text-center mt-3 mb-4">Edit Data Pegawai</h1>
+        <div class="container">
+            
+            <div class="row justify-content-center">
+                <div class="col-8">
+                    <div class="card">
+                        <div class="card-body">
+                         <form action="/updatedata/{{ $data->id }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                             <div class="mb-3">
+                               <label for="exampleInputEmail1" class="form-label">Nama Lengkap</label>
+                               <input type="text" name="nama" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{ $data->nama }}">
+                             </div>
+                             <div class="mb-3">
+                               <label for="exampleInputEmail1" class="form-label">Jenis Kelamin</label>
+                               <select class="form-select" name="jeniskelamin" aria-label="Default select example">
+                                <option selected>{{ $data->jeniskelamin }}</option>
+                                <option value="cowok">Cowok</option>
+                                <option value="cewek">Cewek</option>
+                              </select>
+                             </div>
+                             <div class="mb-3">
+                               <label for="exampleInputEmail1" class="form-label">No Telpon</label>
+                               <input type="number" name="notelpon" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  value="{{ $data->notelpon }}">
+                             </div>
+                             <button type="submit" class="btn btn-primary">Update</button>
+                           </form>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
